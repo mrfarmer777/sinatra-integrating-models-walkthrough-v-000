57053590a1838d9f30602570122caaa009ab5361
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 02:04:24 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,16 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+
+  def initialize(text)
+    @text=text.downcase
+  end
+
+  def count_of_words
+    words=text.split(" ")
+    words.count
+  end
+
+  def count_of_vowels
+    text.scan(/[aeoui]/).count
+  end
